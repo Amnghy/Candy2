07e1a46f335db8d77d825678a9d7614c9b2ea017
Amnghy <1076776375@outlook.com>
2022-02-16 09:37:52 +0800

    22

---
+++ src/components/beans/Valine.vue
@@ -0,0 +1,28 @@
+<template>
+ <div id="vcomments"></div>
+</template>
+ 
+<script>
+export default {
+ name: 'Valine',
+ mounted: function(){
+  // require window 
+  const Valine = require('valine');
+  if (typeof window !== 'undefined') {
+   this.window = window
+   window.AV = require('leancloud-storage')
+    
+  }
+    
+  new Valine({
+   el: '#vcomments' ,
+   appId: 'A9E0wglWeUDqd0bQPfrXnEFh-MdYXbMMI',// your appId
+   appKey: 'x5JipUK8WxuVpiA38rAuYYh7', // your appKey
+   notify:false, 
+   verify:false, 
+   avatar:'mm', 
+   placeholder: 'just go go'
+  });
+ },
+}
+</script>
